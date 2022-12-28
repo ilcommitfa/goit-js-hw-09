69cd7964550690b757163fd11b0fe6e634d39267
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2022-12-28 17:32:25 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let interval;
 
+startBtn.addEventListener('click', changeBodyColor);
+stopBtn.addEventListener('click', stopChangeBodyColor);
+buttonsGrowPlease();
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeBodyColor() {
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+};
+
+function stopChangeBodyColor() {
+  clearInterval(interval);
+  startBtn.disabled = false;
+};
