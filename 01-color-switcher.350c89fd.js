9a868b5b9a73f93d07645c9d7f02e0018a3cfccf
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2022-12-28 15:33:06 +0000

    Deploying to gh-pages from @ ilcommitfa/goit-js-hw-09@69cd7964550690b757163fd11b0fe6e634d39267 🚀

---
+++ 01-color-switcher.350c89fd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o;t.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1})),buttonsGrowPlease();
+//# sourceMappingURL=01-color-switcher.350c89fd.js.map
---
+++ 01-color-switcher.f662a7f0.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1})),buttonsGrowPlease()}();
+//# sourceMappingURL=01-color-switcher.f662a7f0.js.map
